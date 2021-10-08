af24fce01e30c473155fb3a2c28f092a4924d4e3
YI Eun Gook <eungook83@gmail.com>
2021-10-08 22:39:41 +0900

    yarn add react-router-dom, 기본 <Router /> 생성

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { RecoilRoot } from 'recoil';
 import './index.css';
-import App from './App';
+import Router from './Router';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <Router />
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
