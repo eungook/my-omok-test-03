550150b3097d7923881a06247835dc43440a4236
YI Eun Gook <eungook83@gmail.com>
2021-10-18 20:36:39 +0900

    <Room />: 방번호와 돌 색깔 출력

--- src/Room.js
+++ src/Room.js
@@ -1,9 +1,13 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { colorState } from './atom';
 import Board from './Board';
 
 function Room() {
 	const { roomKey } = useParams();
+	const [color, setColor] = useRecoilState(colorState); // B: 검은 돌, W: 흰 돌
+
 	useEffect(() => {
 		console.log({
 			'where': 'Room()',
@@ -14,7 +18,8 @@ function Room() {
 	return (
 		<div>
 			<h3>오목방</h3>
-			<h4>방번호: </h4>
+			<h4>방번호: {roomKey}</h4>
+			<h4>나의 돌 색깔: {(color == 'W') ? '흰 돌' : '검은 돌'}</h4>
 			<Board />
 		</div>
 	);
